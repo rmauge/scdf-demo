87af76df3439fbd22cbdb74bce51efdaff18e393
Raymond Maugé <rmauge@gmail.com>
2021-04-27 21:54:58 -0400

    Uses Java 11 and functional streams

--- api-reader-source/src/main/java/com/rmauge/scdf/demo/apisource/Sender.java
+++ api-reader-source/src/main/java/com/rmauge/scdf/demo/apisource/Sender.java
@@ -5,33 +5,24 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@EnableScheduling
-@EnableBinding(Source.class)
+import java.util.function.Supplier;
+
+@Configuration
 public class Sender {
+    private final Log log = LogFactory.getLog(Sender.class);
 
-    private final Source source;
-    private final RestTemplate restTemplate;
-    private final String polledURL;
-    private Log log = LogFactory.getLog(Sender.class);
-
-    public Sender(final Source source, final RestTemplateBuilder restTemplateBuilder, @Value("${polledURL}") final String polledURI) {
-        this.source = source;
-        this.restTemplate = restTemplateBuilder.build();
-        this.polledURL = polledURI;
-        log.info("polledURL:" + polledURI);
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    public void sendEvents() {
-        Signal signal = restTemplate.getForObject(polledURL, Signal.class);
-
-        this.source.output().send(MessageBuilder.withPayload(signal).build());
+    @Bean
+    public Supplier<Signal> signalSource(final RestTemplateBuilder restTemplateBuilder, @Value("${polledURL}") final String polledURL) {
+        final RestTemplate restTemplate = restTemplateBuilder.build();
+        return () -> {
+            log.info("Sending signal");
+            final Signal signal = restTemplate.getForObject(polledURL,
+                    Signal.class);
+            return signal;
+        };
     }
 }
--- signal-processor/src/main/java/com/rmauge/scdf/demo/signalprocessor/SignalProcessor.java
+++ signal-processor/src/main/java/com/rmauge/scdf/demo/signalprocessor/SignalProcessor.java
@@ -2,19 +2,16 @@ package com.rmauge.scdf.demo.signalprocessor;
 
 import com.rmauge.scdf.demo.lib.models.Signal;
 import com.rmauge.scdf.demo.lib.models.UserSignalDetails;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
 
-@EnableBinding(Processor.class)
+@Configuration
 public class SignalProcessor {
-
-    private static int[] customers = {1001, 1002, 1003};
 
     private static Map<Integer, String> custMap = new HashMap<>();
 
@@ -24,15 +21,16 @@ public class SignalProcessor {
         custMap.put(1003, "crmCust3");
     }
 
-    @StreamListener(Processor.INPUT)
-    @SendTo(Processor.OUTPUT)
-    public UserSignalDetails processSignal(Signal signal) {
-        UserSignalDetails userSignalDetails = new UserSignalDetails();
-        userSignalDetails.setUserId(signal.getCustId());
-        userSignalDetails.setSalesForceId(custMap.get(signal.getCustId()));
-        userSignalDetails.setScore(calScore(signal.getCustId(), signal.getValue()));
+    @Bean
+    public Function<Signal, UserSignalDetails> processSignal() {
+        return signal -> {
+            UserSignalDetails userSignalDetails = new UserSignalDetails();
+            userSignalDetails.setUserId(signal.getCustId());
+            userSignalDetails.setSalesForceId(custMap.get(signal.getCustId()));
+            userSignalDetails.setScore(calScore(signal.getCustId(), signal.getValue()));
 
-        return userSignalDetails;
+            return userSignalDetails;
+        };
     }
 
     /**
